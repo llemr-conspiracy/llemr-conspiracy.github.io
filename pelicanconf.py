fd4791dc10a0ea77e8aeb54786c6f13534362e72
Justin R. Porter <justinrporter@gmail.com>
2021-06-03 17:24:05 -0500

    initial commit.

---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'The LLEMR Conspiracy'
+SITENAME = 'The LLEMR Conspiracy'
+SITESUBTITLE = 'LLEMR is a Lightweight EMR'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Chicago'
+
+DEFAULT_LANG = 'en'
+
+THEME = 'themes/notmyidea'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),)
+
+# Social widget
+SOCIAL = (('github', 'http://github.com/llemr-conspiracy'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
